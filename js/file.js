b82cee85caec76a7119280f15dab0ab40440b7c9
Shyamcode01 <140502459+Shyamcode01@users.noreply.github.com>
2023-08-01 12:20:40 +0530

    secound update

---
+++ js/file.js
@@ -0,0 +1,7 @@
+
+let btn=document.querySelector(".btnmenu").addEventListener("click",btnc=>{
+   
+   let c=document.querySelector(".menubar").classList.toggle("btnclous");
+     
+    console.log(c);
+})
